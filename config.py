3b6596873deed773e5b53d692b53a234e8f22e08
pstetz <pstetz@berkeley.edu>
2021-08-14 13:35:52 -0700

    config for project

---
+++ config.py
@@ -0,0 +1,9 @@
+# The config is customizable to different options.
+# Make sure each list has the same length!
+config = {
+    "initial": [10**5, 10**6],
+    "avg_interest": [0.07, 0.08],
+    "inflation": [0.02, 0.025],
+    "monthly_contributions": [200, 100],
+    "time_horizon": [40, 45]
+}
