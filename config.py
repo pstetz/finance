875f576d55d58836e5bf2f8dd595be7545c1e8e3
pstetz <pstetz@berkeley.edu>
2021-08-14 13:48:01 -0700

    modify config variable names

--- config.py
+++ config.py
@@ -2,8 +2,7 @@
 # Make sure each list has the same length!
 config = {
     "initial": [10**5, 10**6],
-    "avg_interest": [0.07, 0.08],
-    "inflation": [0.02, 0.025],
-    "monthly_contributions": [200, 100],
-    "time_horizon": [40, 45]
+    "rate": [0.07, 0.08],
+    "monthly": [200, 100],
+    "years": [40, 45]
 }
